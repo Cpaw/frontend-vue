96da56c492e530091613c2ea61519bff0d3790a1
kuroiwasi <thermite.process@gmail.com>
2016-08-04 09:36:52 +0900

    questions-1

---
+++ src/components/Questions.vue
@@ -0,0 +1,208 @@
+<template>
+    <article>
+        <div class="container">
+            <div class="qcontents bg-field">
+                <div class="genrenavbar">
+                    <nav>
+                        <ul class="genrenav">
+                            <li><a class="current" href="">GrassLand</a></li>
+                            <li><a href="">Mountain</a></li>
+                            <li><a href="">Volcano</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="readable">
+                    <div class="smallbox ">
+                        <h2>Web - Grassland</h2>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    
+.readable
+{
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  margin-bottom: 5px;
+  padding: 0;
+  background-color: rgba(34,34,34,0.7);
+  color: rgb(200,200,200);
+}
+
+.readable h2
+{
+    margin:0;
+    color: rgb(200,200,200);
+}
+
+.smallbox
+{
+    width:90%;
+    height: 90%;
+    margin: 0 auto;
+    padding-top: 1em;
+    padding-bottom: 1em;
+}
+
+.aQuestion
+{
+  min-height: 50px;
+  width: 100%;
+  margin-top: 20px;
+  border: 2px solid #33ddff;
+  border-radius: 90px 0 90px 0;
+  color: #fff;
+  background-color: rgba(0,0,0,0.4);
+  clear: both;
+}
+
+.solvedquestion
+{
+  min-height: 50px;
+  width: 100%;
+  margin-top: 20px;
+  border: 2px solid #115588;
+  border-radius: 90px 0 90px 0;
+  color: #aaa;
+  background-color: rgba(0,0,0,0.4);
+  clear: both;  
+}
+
+.aQuestion img
+{
+  display: inline-block;
+  margin-top:-16px;
+  width: 64px;
+  height: 64px;
+  border: 2px solid #33ddff;
+  background-color: rgba(0,0,0,0.3);
+}
+
+.solvedquestion img
+{
+  display: inline-block;
+  margin-top:-16px;
+  width: 64px;
+  height: 64px;
+  border: 2px solid #115588;
+  background-color: rgba(0,0,0,0.3);
+}
+
+.qitem
+{
+  height: 50px;
+  line-height: 50px;
+  display: block;
+  float: left;
+}
+
+.qicon
+{
+  text-align: center;
+}
+
+.width05 {width: 5%;}
+.width10 {width: 10%;}
+.width15 {width: 15%;}
+.width20 {width: 20%;}
+.width30 {width: 30%;}
+.width35 {width: 35%;}
+.width40 {width: 40%;}
+.width45 {width: 45%;}
+.width50 {width: 50%;}
+
+.aQuestionHead
+{
+  min-height: 50px;
+  width: 100%;
+  margin-top: 20px;
+  border-bottom: 2px solid #33ddff;
+  color: #fff;
+  clear: both;
+}
+
+.stBarWrapper
+{
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;
+  border-bottom: 2px solid #222222;
+  background-color: #333333;
+  color: #000;
+}
+
+.stBar
+{
+  height: 15px;
+  border-radius: 5px;
+  background-color: #11ff22;
+  float: left;
+  line-height: 15px;
+  color: #000;
+  padding-left: 1em;
+}
+
+/*****************
+ GENRE NAVIGATION
+*****************/
+
+.genrenavbar
+{
+    background-color: #333;
+}
+
+ul.genrenav {
+    height: 100%;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+ul.genrenav li 
+{
+    float: left;
+}
+
+ul.genrenav li a 
+{
+    display: inline-block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+ul.genrenav li a.current
+{
+    background-color: #666;    
+}
+
+ul.genrenav li a:hover 
+{
+    background-color: #555;
+}
+
+@media screen and (max-width:680px) 
+{
+  ul.genrenav {position: relative;}
+  ul.genrenav li {
+    float: none;
+    display: inline;
+  }
+  ul.genrenav li a {
+    display: block;
+    text-align: left;
+  }
+}
+</style>
