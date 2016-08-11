e08d44d4384f66ee70c40d8e479082a9f748fe66
forisel <thermite.process@gmail.com>
2016-08-11 18:53:22 +0900

    matigaete kesitanode hukkatu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import Home from './components/Home'
 import About from './components/About'
 import Questions from './components/Questions'
@@ -8,6 +9,7 @@ import Ranking from './components/Ranking'
 import Notice from './components/Notice'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 var router = new VueRouter()
 
