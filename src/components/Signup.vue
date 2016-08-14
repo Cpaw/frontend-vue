a95e52ac0adff0a630206c2b0ac8ec0ca516d3df
palloc <palloc.mknet10@gmail.com>
2016-08-14 22:27:27 +0900

    signup

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,3 +1,18 @@
 <template>
-<h1>test signup</h1>
+    <article>
+        <div class="container">
+	    <div class="contents">
+	        <h1>test signup</h1>
+	    </div>
+	</div>
+    </article>
 </template>
+
+<script>
+</script>
+
+<style scoped>
+h1 {
+    color: white;
+}
+</style>
