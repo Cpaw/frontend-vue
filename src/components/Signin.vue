303671edd769c6eeff5dc5d73c361dd37ebacace
palloc <palloc.mknet10@gmail.com>
2016-08-16 22:12:25 +0900

    add v-model and some function

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -7,12 +7,14 @@
                     <form method="post" action="localhost:3000/login">
                         <div class="field">
 			    <label for="signin-name">Email: <br></label>
-			    <input id="signin-name" type="text" name="email" placeholder="email">
+			    <input id="signin-name" type="text" name="email" placeholder="email" v-model="user.email">
                         </div>
                         <div class="field">
                             <label for="signin-name">Password: <br></label>
-                            <input id="signin-name" type="text" name="email" placeholder="password">
+                            <input id="signin-name" type="text" name="email" placeholder="password" v-model="user.password">
                         </div>
+			<div class="field>
+			  <button v-on:click="login">Sign in</button>
 		    </form>
 		</div>
 	    </div>
@@ -21,6 +23,38 @@
 </template>
 
 <script>
+import cookie from 'cookie-cutter'
+export default {
+  ready () {
+    if (typeof cookie.get('token') !== 'undefined') {
+      this.$route.router.go('/user')
+    }
+  },
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login: function () {
+      this.$http.post(
+        this.$parent.backend + 'login',
+        {
+          'email': this.user.email,
+          'password': this.user.password
+        }
+      ).then((response) => {
+        this.$route.router.go('/home')
+      }, (response) => {
+        console.log('error')
+        this.$route.router.go('/signin')
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
