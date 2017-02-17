ab13dd74089ea38f02c30c36968740e18a45fca8
palloc <palloc.mknet10@gmail.com>
2017-02-17 07:28:06 +0000

    ルールを追加

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,24 @@
     <div class="container">
       <div class="contents">
         <img class="toplogo" src="/static/img/toplogo.png">
+	<div class="readable">
+	  <div class="smallbox">
+	  <pre>
+	  <h3>Rule:</h3>
+	  ・Don’t attack this score-server.<br>
+	  ・This CTF is unlimited. So, please publish writeup just in local place.
+
+	  <h3>Server:</h3>
+	  ・Made by palloc, nomuken, Schwarz_Sardine
+
+	  <h4>Problem:</h4>
+	  ・Made by palloc, chihiro, ariake<br>
+	  ・Reviewed by n4nu, takahoyo<br>
+	  <br>
+	  If you have some questions, please ask to cpaw.createam[at]gmail.com
+	  </pre>
+	  </div>
+	</div>
       </div>
     </div>
   </article>
@@ -13,7 +31,11 @@ img.toplogo {
   position: absoluted;
   overflow: hidden;
   display: block;
-  width: 90%;
+  width: 80%;
   margin: 0 auto;
+}
+
+pre {
+  width: 60%;
 }
 </style>
